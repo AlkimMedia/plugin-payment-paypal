e3acf19de9dad647bb4f688f70e8d08fc65dbae8
Jens Konopka <jens.konopka@plentymarkets.com>
2016-11-04 09:55:47 +0100

    change route names

--- src/providers/PayPalRouteServiceProvider.php
+++ src/providers/PayPalRouteServiceProvider.php
@@ -17,9 +17,11 @@ class PayPalRouteServiceProvider extends RouteServiceProvider
 	public function map(Router $router)
 	{
 		// Get the PayPal success and cancellation URLs
-		$router->get('plentyPayPal/payPalCheckoutSuccess', 'PayPal\Controllers\PaymentController@payPalCheckoutSuccess');
-		$router->get('plentyPayPal/payPalCheckoutCancel' , 'PayPal\Controllers\PaymentController@payPalCheckoutCancel' );
+		$router->get('payPal/checkoutSuccess', 'PayPal\Controllers\PaymentController@payPalCheckoutSuccess');
+		$router->get('payPal/checkoutCancel' , 'PayPal\Controllers\PaymentController@payPalCheckoutCancel' );
 
-        $router->get('paypalExpressCheckoutAction', 'PayPal\Controllers\PaymentController@payPalExpressCheckout');
+        $router->get('payPal/expressCheckoutAction', 'PayPal\Controllers\PaymentController@payPalExpressCheckout');
+
+        $router->post('payPal/notification', 'PayPal\Controllers\PaymentNotificationController@handleNotification');
 	}
 }
