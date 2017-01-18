c19b5fa59d3047d822933a1be33a2778117ab26e
Jens Konopka <jens.konopka@plentymarkets.com>
2017-01-18 10:33:39 +0100

    Update PayPal installment twig template to use the item data

--- src/Providers/DataProvider/Installment/PayPalInstallmentSpecificPromotion.php
+++ src/Providers/DataProvider/Installment/PayPalInstallmentSpecificPromotion.php
@@ -10,24 +10,42 @@ namespace PayPal\Providers\DataProvider\Installment;
 
 use PayPal\Services\PayPalInstallmentService;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
+use Plenty\Modules\Item\DataLayer\Models\Record;
 use Plenty\Plugin\Templates\Twig;
 
 class PayPalInstallmentSpecificPromotion
 {
     public function call(   Twig $twig,
                             BasketRepositoryContract $basketRepositoryContract,
-                            PayPalInstallmentService $payPalInstallmentService)
+                            PayPalInstallmentService $payPalInstallmentService,
+                            $arg)
     {
-        $basket = $basketRepositoryContract->load();
+        $item = null;
+        if(is_array($arg) && array_key_exists(0, $arg))
+        {
+            /** @var Record $item */
+            $item = $arg[0];
+        }
+        $amount = 0;
+        if(is_null($item))
+        {
+            $basket = $basketRepositoryContract->load();
+            $amount = $basket->basketAmount;
+        }
+        elseif (!is_null($item))
+        {
+            $amount = $item->getVariationRetailPrice()->getPrice();;
+        }
+
         $qualifyingFinancingOptions = [];
 
         // TODO Load Config to check show with or without calculated financing options
-        if(true)
+        if(false)
         {
             /**
              * Load the specific promotion with calculated financing options
              */
-            $financingOptions = $payPalInstallmentService->getFinancingOptions($basket->basketAmount);
+            $financingOptions = $payPalInstallmentService->getFinancingOptions($amount);
             if(is_array($financingOptions) && array_key_exists('financing_options', $financingOptions))
             {
                 if(is_array($financingOptions['financing_options'][0]) && is_array(($financingOptions['financing_options'][0]['qualifying_financing_options'])))
@@ -36,7 +54,7 @@ class PayPalInstallmentSpecificPromotion
                 }
             }
         }
-        return $twig->render('PayPal::PayPalInstallment.SpecificPromotion', ['basketAmount'=>$basket->basketAmount, 'financingOptions'=>$qualifyingFinancingOptions, 'merchantName'=>'Testfirma', 'merchantAddress'=>'Teststraße 1, 34117 Kassel']);
+        return $twig->render('PayPal::PayPalInstallment.SpecificPromotion', ['amount'=>$amount, 'financingOptions'=>$qualifyingFinancingOptions, 'item'=>$item,'merchantName'=>'Testfirma', 'merchantAddress'=>'Teststraße 1, 34117 Kassel']);
     }
 
 }
