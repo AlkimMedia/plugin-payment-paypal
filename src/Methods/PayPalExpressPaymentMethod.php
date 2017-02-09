01aa1098b6b4499eea81d9184e615f40b4bb211b
Jens Konopka <jens.konopka@plentymarkets.com>
2017-02-09 11:03:32 +0100

    Update pp express button

--- src/Methods/PayPalExpressPaymentMethod.php
+++ src/Methods/PayPalExpressPaymentMethod.php
@@ -2,8 +2,6 @@
 
 namespace PayPal\Methods;
 
-use PayPal\Services\PaymentService;
-use Plenty\Modules\Frontend\Contracts\Checkout;
 use Plenty\Modules\Payment\Method\Contracts\PaymentMethodService;
 
 /**
@@ -13,47 +11,12 @@ use Plenty\Modules\Payment\Method\Contracts\PaymentMethodService;
 class PayPalExpressPaymentMethod extends PaymentMethodService
 {
     /**
-     * @var Checkout
-     */
-    private $checkout;
-
-    /**
-     * @var PaymentService
-     */
-    private $paymentService;
-
-    /**
-     * PayPalExpressPaymentMethod constructor.
-     *
-     * @param Checkout $checkout
-     * @param PaymentService $paymentService
-     */
-    public function __construct(Checkout                    $checkout,
-                                PaymentService              $paymentService)
-    {
-        $this->checkout         = $checkout;
-        $this->paymentService   = $paymentService;
-        $this->paymentService->loadCurrentSettings('paypal');
-    }
-
-    /**
      * Check whether PayPal Express is active
      *
      * @return bool
      */
     public function isActive():bool
     {
-        /**
-         * Check the allowed shipping countries
-         */
-        if(array_key_exists('shippingCountries', $this->paymentService->settings))
-        {
-            $shippingCountries = $this->paymentService->settings['shippingCountries'];
-            if(is_array($shippingCountries) && in_array($this->checkout->getShippingCountryId(), $shippingCountries))
-            {
-                return true;
-            }
-        }
         return false;
     }
 }
--- src/Providers/DataProvider/PayPalExpressButtonDataProvider.php
+++ src/Providers/DataProvider/PayPalExpressButtonDataProvider.php
@@ -2,6 +2,8 @@
 
 namespace PayPal\Providers\DataProvider;
 
+use PayPal\Services\PaymentService;
+use Plenty\Modules\Frontend\Contracts\Checkout;
 use Plenty\Plugin\Templates\Twig;
 
 /**
@@ -12,12 +14,29 @@ class PayPalExpressButtonDataProvider
 {
     /**
      * @param Twig $twig
+     * @param PaymentService $paymentService
+     * @param Checkout $checkout
      * @param $args
      * @return string
      */
-    public function call(   Twig $twig,
+    public function call(   Twig            $twig,
+                            PaymentService  $paymentService,
+                            Checkout        $checkout,
                             $args)
     {
-        return $twig->render('PayPal::PayPalExpress.PayPalExpressButton');
+        $paymentService->loadCurrentSettings('paypal');
+        /**
+         * Check the allowed shipping countries
+         */
+        if(array_key_exists('shippingCountries', $paymentService->settings))
+        {
+            $shippingCountries = $paymentService->settings['shippingCountries'];
+            if(is_array($shippingCountries) && in_array($checkout->getShippingCountryId(), $shippingCountries))
+            {
+                return $twig->render('PayPal::PayPalExpress.PayPalExpressButton');
+            }
+        }
+
+        return '';
     }
 }
