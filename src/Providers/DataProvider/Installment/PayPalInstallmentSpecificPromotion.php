f7a7b724764f906b1bc6ba4ac9701ee17250db02
Jens Konopka <jens.konopka@plentymarkets.com>
2017-02-28 10:04:24 +0100

    Add unique modal id Add financing plan sorting

--- src/Providers/DataProvider/Installment/PayPalInstallmentSpecificPromotion.php
+++ src/Providers/DataProvider/Installment/PayPalInstallmentSpecificPromotion.php
@@ -12,6 +12,7 @@ use PayPal\Services\PaymentService;
 use PayPal\Services\PayPalInstallmentService;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Plenty\Modules\Item\DataLayer\Models\Record;
+use Plenty\Modules\Item\SalesPrice\Models\SalesPriceSearchResponse;
 use Plenty\Plugin\Templates\Twig;
 
 class PayPalInstallmentSpecificPromotion
@@ -22,59 +23,75 @@ class PayPalInstallmentSpecificPromotion
                             PaymentService $paymentService,
                             $arg)
     {
+        $id = '';
         $item = null;
+        $amount = 0;
         if(is_array($arg) && array_key_exists(0, $arg))
         {
             /** @var Record $item */
             $item = $arg[0];
         }
-        $amount = 0;
         if(is_null($item))
         {
             $basket = $basketRepositoryContract->load();
             $amount = $basket->basketAmount;
         }
-        elseif (!is_null($item))
+        elseif (!is_null($item) && is_array($item))
         {
-            $amount = $item->getVariationRetailPrice()->getPrice();;
+            $calculatedPrices = $item['calculatedPrices']['default'];
+            if($calculatedPrices instanceof SalesPriceSearchResponse)
+            {
+                $amount = $calculatedPrices->price;
+            }
+
+            if(array_key_exists('variation', $item))
+            {
+                $id = $item['variation']['itemId'].'-'.$item['variation']['id'];
+            }
+            else
+            {
+                $id = $item['item']['id'];
+            }
         }
 
-        $qualifyingFinancingOptions = [];
-
-        $paymentService->loadCurrentSettings('paypal_installment');
-
-        if($paymentService->settings['calcFinancing'] == 1)
+        if($amount > 0)
         {
-            /**
-             * Load the specific promotion with calculated financing options
-             */
-            $financingOptions = $payPalInstallmentService->getFinancingOptions($amount);
-            if(is_array($financingOptions) && array_key_exists('financing_options', $financingOptions))
+            $qualifyingFinancingOptions = [];
+            $paymentService->loadCurrentSettings('paypal_installment');
+            if($paymentService->settings['calcFinancing'] == 1)
             {
-                if(is_array($financingOptions['financing_options'][0]) && is_array(($financingOptions['financing_options'][0]['qualifying_financing_options'])))
+                /**
+                 * Load the specific promotion with calculated financing options
+                 */
+                $financingOptions = $payPalInstallmentService->getFinancingOptions($amount);
+                if(is_array($financingOptions) && array_key_exists('financing_options', $financingOptions))
                 {
-                    /*
-                     * Choose a very high value to check if lower values exist
-                     */
-                    $lowestAPR = 1000000;
-                    /*
-                     * Use the first financing option as fallback
-                     */
-                    $actFinancingOption = $financingOptions['financing_options'][0]['qualifying_financing_options'][0];
-                    foreach ($financingOptions['financing_options'][0]['qualifying_financing_options'] as $qualifying_financing_option)
+                    if(is_array($financingOptions['financing_options'][0]) && is_array(($financingOptions['financing_options'][0]['qualifying_financing_options'])))
                     {
-                        if($qualifying_financing_option['monthly_percentage_rate'] > 0 && $qualifying_financing_option['monthly_percentage_rate'] < $lowestAPR)
+                        /*
+                         * Choose a very high value to check if lower values exist
+                         */
+                        $lowestAPR = 1000000;
+                        /*
+                         * Use the first financing option as fallback
+                         */
+                        $actFinancingOption = $financingOptions['financing_options'][0]['qualifying_financing_options'][0];
+                        foreach ($financingOptions['financing_options'][0]['qualifying_financing_options'] as $qualifying_financing_option)
                         {
-                            $lowestAPR = $qualifying_financing_option['monthly_percentage_rate'];
-                            $actFinancingOption = $qualifying_financing_option;
+                            if($qualifying_financing_option['monthly_percentage_rate'] > 0 && $qualifying_financing_option['monthly_percentage_rate'] < $lowestAPR)
+                            {
+                                $lowestAPR = $qualifying_financing_option['monthly_percentage_rate'];
+                                $actFinancingOption = $qualifying_financing_option;
+                            }
                         }
-                    }
 
-                    $qualifyingFinancingOptions = $actFinancingOption;
+                        $qualifyingFinancingOptions = $actFinancingOption;
+                    }
                 }
             }
         }
-        return $twig->render('PayPal::PayPalInstallment.SpecificPromotion', ['amount'=>$amount, 'financingOptions'=>$qualifyingFinancingOptions, 'item'=>$item,'merchantName'=>'Testfirma', 'merchantAddress'=>'Teststraße 1, 34117 Kassel']);
+
+        return $twig->render('PayPal::PayPalInstallment.SpecificPromotion', ['amount'=>$amount, 'financingOptions'=>$qualifyingFinancingOptions, 'item'=>$item, 'id'=>$id, 'merchantName'=>'Testfirma', 'merchantAddress'=>'Teststraße 1, 34117 Kassel']);
     }
 
 }
