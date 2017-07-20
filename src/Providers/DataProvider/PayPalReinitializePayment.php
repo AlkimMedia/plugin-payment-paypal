4d71cd84a0de3a89fc0909a2f55a5919282103e1
Marcel Pöninghaus <mpoeninghaus@Marcel-Poenninghaus-MacBook-Pro.local>
2017-07-20 15:54:27 +0200

    rename files

--- src/Providers/DataProvider/PayPalReinitializePayment.php
+++ src/Providers/DataProvider/PayPalReinitializePayment.php
@@ -6,12 +6,12 @@ use PayPal\Helper\PaymentHelper;
 use Plenty\Modules\Frontend\Contracts\Checkout;
 use Plenty\Plugin\Templates\Twig;
 
-class PayPalInstallmentReinitializePayment
+class PayPalReinitializePayment
 {
     public function call(Twig $twig, $arg):string
     {
       $paymentHelper = pluginApp(PaymentHelper::class);
       $typeId = $paymentHelper->getPayPalMopIdByPaymentKey('PAYPAL');
-      return $twig->render('PayPal::PayPalInstallment.ReinitializePayment', ["order" => $arg[0], "paymentMethodId" => $typeId]);
+      return $twig->render('PayPal::PayPalReinitializePayment', ["order" => $arg[0], "paymentMethodId" => $typeId]);
     }
 }
--- src/Providers/DataProvider/PayPalReinitializePaymentScript.php
+++ src/Providers/DataProvider/PayPalReinitializePaymentScript.php
@@ -5,13 +5,13 @@ namespace PayPal\Providers\DataProvider\Installment;
 use Plenty\Plugin\Templates\Twig;
 use PayPal\Helper\PaymentHelper;
 
-class PayPalInstallmentReinitializePaymentScript
+class PayPalReinitializePaymentScript
 {
 
     public function call(Twig $twig):string
     {
       $paymentHelper = pluginApp(PaymentHelper::class);
       $typeId = $paymentHelper->getPayPalMopIdByPaymentKey('PAYPAL');
-      return $twig->render('PayPal::PayPalInstallment.ReinitializePaymentScript', ['typeId' => $typeId]);
+      return $twig->render('PayPal::PayPalReinitializePaymentScript', ['typeId' => $typeId]);
     }
 }
